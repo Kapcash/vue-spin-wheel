07e5e7beb839853445488d95c96b3620aae475e0
Florent Catiau-Tristant <florent.ct@orange.fr>
2020-02-25 13:49:47 +0100

    chore: remove @vue/prettier, it bother me.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ["plugin:vue/essential", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
